b6914008f622b7a98ebb0406ae28702b0874bd8a
Thomas Puntillo <48040854+tom-puntillo@users.noreply.github.com>
2023-08-03 23:54:23 -0400

    added backend block in terraform.tf (#12)

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -1,4 +1,9 @@
 terraform {
+  backend "s3" {
+    bucket = "my-terraform-state-tjp"
+    key    = "terraform/mystate"
+    region = "us-east-1"
+  }
   required_version = ">= 1.0.0"
   required_providers {
     aws = {
