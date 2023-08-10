3928687236a67eeff5d7b9a53c4a4ae2c54a838c
Thomas Puntillo <48040854+tom-puntillo@users.noreply.github.com>
2023-08-09 23:46:48 -0400

    added data types and built-in function files (#19)

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -90,3 +90,19 @@ variable "env" {
     }
   }
 }
+
+variable "num_1" {
+  type        = number
+  description = "Numbers for function labs"
+  default     = 88
+}
+variable "num_2" {
+  type        = number
+  description = "Numbers for function labs"
+  default     = 73
+}
+variable "num_3" {
+  type        = number
+  description = "Numbers for function labs"
+  default     = 52
+}
