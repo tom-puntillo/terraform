a66312e49b547998cea6bd689ccacf2211f769be
tom-puntillo <tom.puntillo@gmail.com>
2023-08-10 04:59:28 +0000

    modified variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -106,3 +106,28 @@ variable "num_3" {
   description = "Numbers for function labs"
   default     = 52
 }
+
+variable "web_ingress" {
+  type = map(object(
+    {
+      description = string
+      port        = number
+      protocol    = string
+      cidr_blocks = list(string)
+    }
+  ))
+  default = {
+    "80" = {
+      description = "Port 80"
+      port        = 80
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+    "443" = {
+      description = "Port 443"
+      port        = 443
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+  }
+}
